29fda179778416743a7513ee59d251b64ce7801d
miabdulrahman <m.i.abdulrahmanar@gmail.com>
2025-02-19 09:57:12 +0530

    Added new file abdul2 and updated the abdul.c file

--- abdul2.c
+++ abdul2.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main() {
   printf("Welcome to the fundumental of the programing\n");
-  printf("Abdul Rahman");
+  printf("Abdul Rahman\n");
+  printf("19-02-2025");
 
   return 0;
 }
